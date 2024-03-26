f4541eda0df5f68555ba6667a138531b75eed4f2
Matias Paez molina <paezmolina@gmail.com>
2024-03-26 13:05:35 -0300

    Minors

--- apps/sociall-api/app/routes/login.jsx
+++ apps/sociall-api/app/routes/login.jsx
@@ -1,20 +1,13 @@
 import { json } from "@remix-run/node";
 import jwt from "jsonwebtoken";
 import { getInstallation } from "../dao.js";
+import { CORS_HTTP_PARAMS } from "../constants";
 
 const secretKey = "matias";
 
-const corsHttpParams = {
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE",
-    "Access-Control-Allow-Headers": "Content-Type",
-  },
-};
-
 export const loader = async ({ request }) => {
   if (request.method === `OPTIONS`) {
-    return json("", corsHttpParams);
+    return json("", CORS_HTTP_PARAMS);
   }
 
   return json({ error: "Invalid request method" }, { status: 405 });
