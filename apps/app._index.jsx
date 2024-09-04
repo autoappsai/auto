219916513ddb94e22dbe40aa42366f1556c652a6
Christian Morán <cmoran4@llbean.com>
2024-09-04 14:18:13 -0300

    Add minor changes

--- apps/app._index.jsx
+++ apps/app._index.jsx
@@ -80,7 +80,7 @@ export default function Index() {
   }
 
   async function authFB() {
-    initTokenFlow(loaderData.INST_SN_ID);
+    await initTokenFlow(loaderData.INST_SN_ID);
 
     var url = `https://www.facebook.com/v19.0/dialog/oauth?client_id=761954749115582&display=popup&redirect_uri=https://auto-testing-remix-js.vercel.app/oauth&response_type=token&scope=instagram_basic,instagram_content_publish,instagram_manage_comments,instagram_manage_insights,pages_show_list,pages_read_engagement,page_events,pages_manage_cta`;
     var width = 600;
