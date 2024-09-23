a9ab72367cd54863785552ad1df70c28d407a0ee
Juan <team@autoapps.ai>
2024-09-23 15:51:55 -0300

    Add GA

--- apps/sociall/app/root.jsx
+++ apps/sociall/app/root.jsx
@@ -24,25 +24,25 @@ export default function App() {
 				<meta name="viewport" content="width=device-width,initial-scale=1" />
 				<Meta />
 				<Links />
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M572N9P2')`,
+					}}
+				/>
 			</head>
 			<body>
 				<Outlet />
 				<LiveReload />
 				<Scripts />
 				<ScrollRestoration />
-				{/* Insert Tawk.to script before closing body tag */}
 				<script
 					dangerouslySetInnerHTML={{
-						__html: `
-						var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-						(function(){
-						var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-						s1.async=true;
-						s1.src='https://embed.tawk.to/66f19b4f4cbc4814f7dd9c93/1i8fsdbn3';
-						s1.charset='UTF-8';
-						s1.setAttribute('crossorigin','*');
-						s0.parentNode.insertBefore(s1,s0);
-						})();`,
+						__html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M572N9P2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
 					}}
 				/>
 			</body>
